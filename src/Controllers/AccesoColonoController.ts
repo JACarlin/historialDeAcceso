f25ecc16d83dc94e298ce02394e964d6c79220e2
CristianRC03 <crizcamach2003@gmail.com>
2023-04-21 15:57:29 -0600

    Update AccesoColonoController.ts

--- src/Controllers/AccesoColonoController.ts
+++ src/Controllers/AccesoColonoController.ts
@@ -1,4 +1,14 @@
+import { Request, Response } from 'express';
+import AccesoColono from '../Models/AccesoColono';
 import Controller from './Controller';
+
+interface registrarAccesoColono {
+    idColono : Number,
+    nombreColono : String,
+    ingreso: Boolean,
+    fechayHoraAcceso: Date,
+    direccion : String
+}
 
 export default class AccesoColonoController extends Controller {
     public constructor() {
@@ -6,6 +16,28 @@ export default class AccesoColonoController extends Controller {
     }
 
     protected initializeRouter(): void {
-        this.router.post('/registro');
+        this.router.post('/registrar');
+    }
+
+    private async registrar(req: Request, res: Response) {
+        try 
+        {
+            const { 
+                idColono, 
+                nombreColono, 
+                ingreso, 
+                fechayHoraAcceso, 
+                direccion 
+            } = <registrarAccesoColono>req.body;
+
+            if (!idColono || !nombreColono || !ingreso || !fechayHoraAcceso || !direccion) {
+                res.status(400).end('Falta algún atributo');
+                return;
+            }
+
+            const accesoColono = new AccesoColono( idColono, nombreColono, fechayHoraAcceso, direccion, ingreso );
+
+            accesoColono.save();
+        }
     }
 }

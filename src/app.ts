44d701f2ede29f5b45ed37bc14c35686d7f18e14
CristianRC03 <crizcamach2003@gmail.com>
2023-04-22 00:33:05 -0600

    Last version

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-import mongoose from "mongoose";
 import AccesoColonoController from "./Controllers/AccesoColonoController";
 import AppBuilder from "./AppBuilder";
 import AccesoVisitaController from "./Controllers/AccesoVisitaController";
-
-async function connectMongoose() {
-    await mongoose.connect('mongodb://localhost:27017/ControlHistorial');
-  }
-
-connectMongoose();
 
 const appBuilder = new AppBuilder();
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import app from "./app";
+import mongoose from 'mongoose';
+import app from './app';
 
+async function connectMongoose() {
+    await mongoose.connect('mongodb://localhost:27017/ControlHistorial');
+  }
+
+connectMongoose();
 
 const port = 3001;
 
